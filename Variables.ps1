68c14d263ed90f545a206642c9fb8431c11fe3fa
Simon Kurtz <simonkurtz@microsoft.com>
2023-11-20 08:18:14 -0500

    Update Variables.ps1

--- Variables.ps1
+++ Variables.ps1
@@ -5,8 +5,11 @@ Set-Variable -Scope Global -Name APPINSIGHTS_INSTRUMENTATIONKEY -Value 255a391c-
 Set-Variable -Scope Global -Name AZURE_CONTAINER_REGISTRY_NAME -Value crtaskstracker3fpetx
 Set-Variable -Scope Global -Name AZURE_SUBSCRIPTION_ID -Value 5fb73327-9152-4f64-bf8a-90dc0cc4ad8f
 Set-Variable -Scope Global -Name BACKEND_API_EXTERNAL_BASE_URL -Value https://tasksmanager-backend-api.livelypebble-1933abe8.eastus.azurecontainerapps.io
+Set-Variable -Scope Global -Name BACKEND_API_INTERNAL_BASE_URL -Value https://tasksmanager-backend-api.internal.livelypebble-1933abe8.eastus.azurecontainerapps.io
 Set-Variable -Scope Global -Name BACKEND_API_NAME -Value tasksmanager-backend-api
 Set-Variable -Scope Global -Name ENVIRONMENT -Value cae-tasks-tracker
+Set-Variable -Scope Global -Name FRONTEND_UI_BASE_URL -Value https://tasksmanager-frontend-webapp.livelypebble-1933abe8.eastus.azurecontainerapps.io
+Set-Variable -Scope Global -Name FRONTEND_WEBAPP_NAME -Value tasksmanager-frontend-webapp
 Set-Variable -Scope Global -Name LOCATION -Value eastus
 Set-Variable -Scope Global -Name RANDOM_STRING -Value 3fpetx
 Set-Variable -Scope Global -Name RESOURCE_GROUP -Value rg-tasks-tracker-3fpetx
@@ -16,4 +19,4 @@ Set-Variable -Scope Global -Name WORKSPACE_ID -Value cd80fcd8-4582-4db8-b444-489
 Set-Variable -Scope Global -Name WORKSPACE_NAME -Value log-tasks-tracker-3fpetx
 Set-Variable -Scope Global -Name WORKSPACE_SECRET -Value 6mLVOgmLkkAfHxW++LmnIItUrSGmGIQcltphnwFcud+3d57phlV7e3wYxZ7IaZKgciP73i+5nAxrrzrNQ/EOEw==
 Set-Variable -Scope Global -Name TODAY -Value (Get-Date -Format 'yyyyMMdd')
-Write-Host "Set 17 variables."
+Write-Host "Set 20 variables."
