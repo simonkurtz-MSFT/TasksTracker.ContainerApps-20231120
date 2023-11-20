5ea938c06ea7080e77817d9e31ca519624a6fe38
Simon Kurtz <simonkurtz@microsoft.com>
2023-11-20 10:14:37 -0500

    Add Module 4

--- TasksTracker.TasksManager.Backend.Api/Program.cs
+++ TasksTracker.TasksManager.Backend.Api/Program.cs
@@ -4,7 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddSingleton<ITasksManager, FakeTasksManager>();
+builder.Services.AddDaprClient();
+builder.Services.AddSingleton<ITasksManager, TasksStoreManager>();
+//builder.Services.AddSingleton<ITasksManager, FakeTasksManager>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
